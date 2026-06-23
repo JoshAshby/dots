1dc7ac6e79fd35915273d49aceb143c72ce418a3
JoshAshby <joshuaashby@joshashby.com>
2026-06-23 11:57:07 -0600

    Update for 1password, mise and FF config changes

--- .config/fish/conf.d/mise.fish
+++ .config/fish/conf.d/mise.fish
@@ -1,6 +1,7 @@
 test -e $HOME/.local/bin/mise
 and if status is-interactive
   $HOME/.local/bin/mise activate fish | source
+  fnox activate fish | source
 else
   $HOME/.local/bin/mise activate fish --shims | source
 end
